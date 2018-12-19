a15f636a993002ae55725f0c7d5a8ef6c394f02e
Martin Charles <martincharles07@gmail.com>
2018-12-19 16:59:16 -0500

    Initial Commit

---
+++ app/src/main/java/me/a0xcaff/forte/ConnectActivity.kt
@@ -0,0 +1,12 @@
+package me.a0xcaff.forte
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConnectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect)
+    }
+}
