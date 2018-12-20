dde4eab4a5e1aa2ef482820e49a1189ed4d88fe4
Martin Charles <martincharles07@gmail.com>
2018-12-19 20:09:33 -0500

    Changed Colors and Changed Launcher Activity to Splash

--- app/src/main/java/me/a0xcaff/forte/SplashActivity.kt
+++ app/src/main/java/me/a0xcaff/forte/SplashActivity.kt
@@ -3,10 +3,9 @@ package me.a0xcaff.forte
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class ConnectActivity : AppCompatActivity() {
-
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_connect)
+        setContentView(R.layout.activity_splash)
     }
 }
