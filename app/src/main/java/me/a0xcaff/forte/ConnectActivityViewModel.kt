e8eef5868a1f4fbd6832b804f039e186d44ba64a
Martin Charles <martincharles07@gmail.com>
2018-12-21 21:27:48 -0500

    Added Simple GraphQL Query
    This isn't the most organized way to do things.
    
    * The schema should be included into the repo in a versioned way.
    
    * The ApolloClient needs to be somewhere else.
    
    * Should probably add dagger at some point.
    
    * Maybe should be using Rx or Kotlin Coroutines

--- app/src/main/java/me/a0xcaff/forte/ConnectActivityViewModel.kt
+++ app/src/main/java/me/a0xcaff/forte/ConnectActivityViewModel.kt
@@ -3,6 +3,13 @@ package me.a0xcaff.forte
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import me.a0xcaff.forte.graphql.TestQuery
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
 
 class ConnectActivityViewModel : ViewModel() {
     private val _url = MutableLiveData<String>().default("")
@@ -21,19 +28,41 @@ class ConnectActivityViewModel : ViewModel() {
 
     val isLoading: LiveData<Boolean> get() = _isLoading
 
+    private val httpUrl get() = HttpUrl.parse(url.value!!)
+
     fun urlChanged(newValue: String) {
         _url.value = newValue
-        _error.value = if (!newValue.isEmpty() && newValue.length < 3) {
-            "Add more chars!"
+
+        _error.value = if (!url.value!!.isEmpty() && httpUrl == null) {
+            "Invalid URL"
         } else {
             null
         }
 
-        _canConnect.value = newValue.length >= 3
+        _canConnect.value = !url.value!!.isEmpty() && httpUrl != null
     }
 
     fun connect() {
         _isLoading.value = true
         _canConnect.value = false
+
+        val okHttpClient = OkHttpClient.Builder().build()
+
+        val apolloClient = ApolloClient.builder()
+            .serverUrl(httpUrl!!)
+            .okHttpClient(okHttpClient)
+            .build()
+
+        val query = TestQuery.builder().build()
+
+        apolloClient.query(query).enqueue(object : ApolloCall.Callback<TestQuery.Data>() {
+            override fun onFailure(e: ApolloException) {
+                println(e)
+            }
+
+            override fun onResponse(response: Response<TestQuery.Data>) {
+                println(response.data().toString())
+            }
+        })
     }
 }
