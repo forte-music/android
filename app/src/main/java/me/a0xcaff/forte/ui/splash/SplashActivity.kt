623580779a019f9b6075c51a81f3e8fc3c2fe868
Martin Charles <martincharles07@gmail.com>
2018-12-27 00:04:46 -0500

    Recommend Splash Screen Things
    https://www.bignerdranch.com/blog/splash-screens-the-right-way/

--- app/src/main/java/me/a0xcaff/forte/ui/splash/SplashActivity.kt
+++ app/src/main/java/me/a0xcaff/forte/ui/splash/SplashActivity.kt
@@ -5,17 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.a0xcaff.forte.Config
 import me.a0xcaff.forte.LaunchRedirector
-import me.a0xcaff.forte.R
 import me.a0xcaff.forte.ui.connect.ConnectActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
         val config = Config.getConfig(this)
         val redirector = LaunchRedirector(config) {
             startActivity(Intent(this, ConnectActivity::class.java))
+            finish()
         }
 
         lifecycle.addObserver(redirector)
